d65c1ef9ed76bf3421acfa193e1694df2c6c5c60
Andres Marcelo <felipemarcelo156@gmail.com>
2020-10-10 17:46:45 -0500

    Solving second Exercise

--- challenge.py
+++ challenge.py
@@ -3,7 +3,23 @@ def calculateNGrams(text,n):
     ans = [text[i:i+n] for i in range(iterations)]
     return ans;
 
+def mostFrequentNGram(text,n):
+    nGrams = calculateNGrams(text,n)
+    frequencies = {}
+    ans = [0,None]
+    for nGram in nGrams:
+        if (nGram not in frequencies):
+            frequencies[nGram] = 1
+            
+        else:
+            frequencies[nGram] += 1
+            
+        if (ans[0] < frequencies[nGram]):
+            ans[0] = frequencies[nGram]
+            ans[1] = nGram
+    return ans[1];
+    
 def main():
-    print(calculateNGrams("Slang",1))
+    print(mostFrequentNGram("to be or not to be",2))
 main()
     
