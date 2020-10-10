09130e34a8e59b1afe2ca1718fa9448bcb25a6fc
Andres Marcelo <felipemarcelo156@gmail.com>
2020-10-10 17:18:45 -0500

    Fixing First Exercise

--- challenge.py
+++ challenge.py
@@ -1,8 +1,9 @@
 def calculateNGrams(text,n):
-    ans = [text[i:i+n] for i in range(n)]
+    iterations = (len(text) - n) + 1
+    ans = [text[i:i+n] for i in range(iterations)]
     return ans;
 
 def main():
-    print(calculateNGrams("Slang",3))
+    print(calculateNGrams("Slang",1))
 main()
     
